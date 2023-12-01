364c4dd1884621f83b7257fded0cb03f2390eda6
Ergys <ergysxhaollari02@gmail.com>
2023-12-01 23:51:27 +0100

    Creating components needed separately

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LecturersComponent } from './lecturers/lecturers.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CourseComponent } from './courses/course/course.component';
+import { LecturerComponent } from './lecturers/lecturer/lecturer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    LecturersComponent,
+    CoursesComponent,
+    HeaderComponent,
+    FooterComponent,
+    CourseComponent,
+    LecturerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
