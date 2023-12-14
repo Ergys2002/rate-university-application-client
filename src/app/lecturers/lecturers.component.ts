bf3aede92677b7084b2a5d06ba929b3ba1d524f5
Redjon Belishta <140613717+RedjonB@users.noreply.github.com>
2023-12-14 15:22:50 +0100

    Your commit message here

--- src/app/lecturers/lecturers.component.ts
+++ src/app/lecturers/lecturers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HomePageService} from "../services/home-page.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-lecturers',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./lecturers.component.css']
 })
 export class LecturersComponent {
+  numberOfLecturers?: number;
+  numberOfCourses?: number;
+  numberOfStudents?: number;
 
+  constructor(private homeService: HomePageService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.homeService.getNumberOfInstructors().subscribe({
+      next: result => {
+        this.numberOfLecturers = result
+      }
+    })
+    this.homeService.getNumberOfCourses().subscribe({
+      next: result => {
+        this.numberOfCourses = result
+      }
+    })
+    this.homeService.getNumberOfStudents().subscribe({
+      next: result => {
+        this.numberOfStudents = result
+      }
+    })
+  }
 }
