748c64f44361af6383cf1f79383a9b13fde90f5a
Maksimo <MaksimoRamaj@Gmail.com>
2023-12-25 22:51:47 +0100

    SearchBar implemented

---
+++ src/app/services/search-form.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../environments/environment";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Course} from "../models/course.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchFormService {
+
+    searchedCourses : any;
+
+  constructor(private http:HttpClient) { }
+
+    submitApplication(courseName: string) {
+
+      const data = {
+          courseName : courseName
+      }
+
+      const url =  environment.apiBaseUrl + "courses/search";
+
+      this.http.post(url, data).subscribe(
+          {
+              next: value  => {
+                  this.searchedCourses = value;
+                  console.log(value);
+              },
+              error: error => {
+                  console.log("SubmitApp Error");
+                  console.error(error);
+              }
+          }
+      );
+    }
+}
